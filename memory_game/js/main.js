013ce8725939fd5690d165e693328b68ffacb04b
Wicklund Nels <WicklundNels@JohnDeere.com>
2020-07-22 09:50:26 -0500

    created game logic

---
+++ memory_game/js/main.js
@@ -0,0 +1,7 @@
+console.log("Up and running");
+let card1 = "queen";
+let card2 = "queen";
+let card3 = "king";
+let card4 = "king";
+console.log("User flipped " + card1);
+console.log("User flipped " + card4);
