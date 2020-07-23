b73f11de7ecfe0feca8b1edd09e9b69de22ab82d
Wicklund Nels <WicklundNels@JohnDeere.com>
2020-07-23 14:06:51 -0500

    created game logic object

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,4 +1,25 @@
-let cards = ["queen", "queen", "king", "king"];
+const cards = [
+{
+	rank: "queen",
+	suit: "hearts",
+	cardImage: "images/queen-of-hearts.png"
+},
+{
+	rank: "queen",
+	suit: "diamonds",
+	cardImage: "images/queen-of-diamonds.png"
+},
+{
+	rank: "king",
+	suit: "hearts",
+	cardImage: "images/king-of-hearts.png",
+},
+{
+	rank: "king",
+	suit: "diamonds",
+	cardImage: "images/king-of-diamonds.png"
+}
+];
 let cardsInPlay = [];
 function checkForMatch() {
 	if (cardsInPlay[0] === cardsInPlay[1]) {
@@ -8,8 +29,10 @@ function checkForMatch() {
 	}
 }
 function flipCard(cardID) {
-	console.log("user flipped " + cards[cardID]);
-	cardsInPlay.push(cards[cardID]);
+	console.log("user flipped " + cards[cardID].rank); 
+	console.log(cards[cardID].cardImage);
+	console.log(cards[cardID].suit);
+	cardsInPlay.push(cards[cardID].rank);
 	if (cardsInPlay === 2) {
 	 if (cardsInPlay[0] === cardsInPlay[1]) {
 	 	alert("you found a match");
