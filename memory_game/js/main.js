4e49405fb7b51ff5901efa2b09e2877ab8e522b3
Wicklund Nels <WicklundNels@JohnDeere.com>
2020-07-23 11:38:56 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,15 +1,23 @@
 let cards = ["queen", "queen", "king", "king"];
 let cardsInPlay = [];
-let card1 = cards[0];
-cardsInPlay.push(cards[0]);
-console.log("user flipped queen");
-let card2 = cards[2];
-cardsInPlay.push(cards[2]);
-console.log("user flipped king");
-if (cardsInPlay === 2) {
- if (cardsInPlay[0] === cardsInPlay[1]) {
- 	alert("you found a match");
- } else {
- 	alert("sorry, try again");
- }
+function checkForMatch() {
+	if (cardsInPlay[0] === cardsInPlay[1]) {
+		console.log("You've found a match!");
+	} else {
+		console.log("Sorry, try again.");
+	}
 }
+function flipCard(cardID) {
+	console.log("user flipped " + cards[cardID]);
+	cardsInPlay.push(cards[cardID]);
+	if (cardsInPlay === 2) {
+	 if (cardsInPlay[0] === cardsInPlay[1]) {
+	 	alert("you found a match");
+	 } else {
+	 	alert("sorry, try again");
+	 }
+	}	
+	checkForMatch(); 
+}
+flipCard(0); 
+flipCard(2); 
