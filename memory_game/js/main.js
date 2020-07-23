708d194a2b00e0b96a0d414b0f0867668f25101f
Wicklund Nels <WicklundNels@JohnDeere.com>
2020-07-23 10:27:52 -0500

    control flow updates

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -6,12 +6,10 @@ console.log("user flipped queen");
 let card2 = cards[2];
 cardsInPlay.push(cards[2]);
 console.log("user flipped king");
-if cardsInPlay.length === 2 {
- if cardsInPlay[0] === cardsInPlay[2] {
+if (cardsInPlay === 2) {
+ if (cardsInPlay[0] === cardsInPlay[1]) {
  	alert("you found a match");
  } else {
  	alert("sorry, try again");
  }
-} else {
-	alert("choose another card");
 }
